28d27d1b08cf49891c7dd6666830ec6dcb1fde27
mohammad <121767738+mohammadparsa82@users.noreply.github.com>
2024-02-21 06:35:26 -0800

    commit sender signal

--- core/accounts/models.py
+++ core/accounts/models.py
@@ -2,6 +2,8 @@ from django.db import models
 from django.contrib.auth.models import (
     BaseUserManager,AbstractBaseUser,PermissionsMixin)
 from django.utils.translation import ugettext_lazy as _
+from django.db.models.signals import post_save
+from django.dispatch import receiver
 # Create your models here.
 
 class UserManager(BaseUserManager):
@@ -59,6 +61,9 @@ class User(AbstractBaseUser,PermissionsMixin):
         return self.email
     
 class Profile(models.Model):
+    """
+    Custom profile model for our app
+    """
     user = models.ForeignKey(User,on_delete=models.CASCADE)
     first_name = models.CharField(max_length=255)
     last_name = models.CharField(max_length=255)
@@ -68,4 +73,10 @@ class Profile(models.Model):
     updated_date = models.DateTimeField(auto_now=True)
 
     def __str__(self):
-        return self.email
+        return self.user.email
+    
+# Communication between two tables with the help of sender(signal)
+@receiver(post_save,sender=User)
+def save_profile(sender,instance,created,**kwargs):
+    if created:
+        Profile.objects.create(user=instance)
