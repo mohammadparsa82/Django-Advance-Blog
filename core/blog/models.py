5de468c75cbfaa6e00cee60be9ba95aed003ed67
mohammad <121767738+mohammadparsa82@users.noreply.github.com>
2024-02-21 08:42:06 -0800

    create models & admin app blog

--- core/blog/models.py
+++ core/blog/models.py
@@ -1,26 +1,30 @@
 from django.db import models
+from django.contrib.auth import get_user_model
 
-# Create your models here.
+# getting user model object
+User = get_user_model()
+
 
 class Post(models.Model):
     '''
     this is a class to define posts for blog app
     ''' 
     author = models.ForeignKey(User,on_delete=models.CASCADE)
+    title = models.CharField(max_length=255)
     image = models.ImageField(null=True,blank=True)
     content = models.TextField()
     status = models.BooleanField()
     category = models.ForeignKey('Category',on_delete=models.SET_NULL,null=True)
 
     created_date = models.DateTimeField(auto_now_add=True)
-    updated_date = models.DateTimeField(auto_add=True)
+    updated_date = models.DateTimeField(auto_now=True)
     published_date = models.DateTimeField()
 
     def __str__(self):
         return self.title
 
-class Category(models.Model)
-    name = models.CharField(max_lenght=250)
+class Category(models.Model):
+    name = models.CharField(max_length=250)
 
     def __str__(self):
         return self.name
