d9b398326e187779a82e3fd0b2907263710bebed
mohammad <121767738+mohammadparsa82@users.noreply.github.com>
2024-02-22 11:58:55 -0800

    create class base view

--- core/blog/urls.py
+++ core/blog/urls.py
@@ -1,8 +1,11 @@
 from django.urls import path
 from .views import * 
 from django.views.generic import TemplateView
+from django.views.generic.base import RedirectView
+app_name = "blog" 
 
 urlpatterns = [
-    path('fbv-index', indexView ,name='fbv-index'),
-    path('cbv-index',TemplateView.as_view(template_name="index.html",extra_context={"name":"ali"})),
+    path('cbv-index', IndexView.as_view(),name='cbv-index'),
+    path('go-to-django/<int:pk>', RedirectToDjango.as_view(),name='redirect-to-django'),
+    path('post/',Postlist.as_view(),name='post-list'),
 ]
--- core/blog/views.py
+++ core/blog/views.py
@@ -1,7 +1,59 @@
+from typing import Any
+from django.db.models.query import QuerySet
 from django.shortcuts import render
-
+from django.views.generic.base import TemplateView ,RedirectView
+from .models import Post
+from django.shortcuts import get_object_or_404
+from django.views.generic import ListView
 # Create your views here.
+
+
+# Function Base view show a templates
+'''
 def indexView(request):
+    """
+    a funcation based view to show index page
+    """
     name ="ali"
     context = {"name":name}
     return render(request,"index.html",context)
+'''
+
+
+class IndexView(TemplateView):
+    """
+    a class based view to show index page
+    """
+    template_name = 'index.html'
+    def get_context_data(self, **kwargs): 
+        context = super().get_context_data(**kwargs)
+        context["name"] = "ali"
+        context["posts"] = Post.objects.all()
+        return context
+    
+'''FBV for redirect
+from django.shortcuts import redirect
+def redirectToDjango(request):
+    return redirect('https://www.djangoproject.com')
+'''
+
+class RedirectToDjango(RedirectView):
+    url = "https://www.djangoproject.com"
+
+    def get_redirect_url(self, *args, **kwargs):
+        post = get_object_or_404(Post, pk=kwargs['pk'])
+        print(post)
+        return super().get_redirect_url(*args, **kwargs)
+    
+
+class Postlist(ListView):
+    #model = Post
+    #queryset = Post.objects.all()
+
+    context_object_name = 'posts'
+
+    def get_queryset(self):
+        posts = Post.objects.filter(status=True) 
+        return posts
+
+
