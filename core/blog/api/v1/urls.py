37337c582afe1a6457b9b3c9d2ef5b41270a9b0a
mohammad <121767738+mohammadparsa82@users.noreply.github.com>
2024-03-01 06:12:42 -0800

    commit test api

--- core/blog/api/v1/urls.py
+++ core/blog/api/v1/urls.py
@@ -4,5 +4,6 @@ from .views import *
 app_name = "api-v1" 
 
 urlpatterns = [
-    path('post/', postlist, name="post-list"),
+    path('post/', PostList, name="post-list"),
+    path('post/<id>/',PostDetail,name='post-detail')
 ]
--- core/blog/api/v1/views.py
+++ core/blog/api/v1/views.py
@@ -1,6 +1,15 @@
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
+from .serializers  import PostSerializer
+from blog.models import Post
+
 
 @api_view()
-def postlist(request):
+def PostList(request):
     return Response({"name":"mmd"})
+
+@api_view()
+def PostDetail(request,id):
+        post = Post.objects.get(pk=id)
+        serializer = PostSerializer(post)
+        return Response(serializer.data)
