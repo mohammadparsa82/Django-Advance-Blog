b749296f7fb1a1120e06bdb685fc628350679a8e
mohammad <121767738+mohammadparsa82@users.noreply.github.com>
2024-03-03 11:49:07 -0800

    commit
    commit add class post list(api view) permission classes & serializer class

--- core/blog/api/v1/views.py
+++ core/blog/api/v1/views.py
@@ -25,6 +25,8 @@ def PostList(request):
 
 class PostList(APIView):
     # getting a list of posts and creating new posts
+    permission_classes = [IsAuthenticated]
+    serializer_class = PostSerializer
 
     def get(self,request):
         # retrieving a list of posts
