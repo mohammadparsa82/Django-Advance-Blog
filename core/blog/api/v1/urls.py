b3cdb5eb547a648257f1615ee692ac771ed31eae
mohammad <121767738+mohammadparsa82@users.noreply.github.com>
2024-03-07 09:17:53 -0800

    commit DefaultRouter

--- core/blog/api/v1/urls.py
+++ core/blog/api/v1/urls.py
@@ -1,7 +1,11 @@
 from django.urls import path , include
 from . import views
+from rest_framework.routers import DefaultRouter
 
 app_name = "api-v1" 
+
+router = DefaultRouter()
+router.register('post',views.PostViewsSet,basename='post')
 
 urlpatterns = [
     # path('post/', views.PostList, name="post-list"),
@@ -9,5 +13,10 @@ urlpatterns = [
     # path('post/',views.PostList.as_view(),name="post-list"),
     # path('post/<id>/',views.PostDetail.as_view(),name='post-detail'),
     path('post/', views.PostViewsSet.as_view({'get':'list','post':'create'}),name='post-list'),
-    path('post/<int:pk>', views.PostViewsSet.as_view({'get':'retrieve','put':'update','patch':'partial_update','delete':'destroy'}),name='post-detail'),
+    path('post/<int:pk>', views.PostViewsSet.as_view({'get':'retrieve',
+                                                      'put':'update',
+                                                      'patch':'partial_update',
+                                                      'delete':'destroy'}),name='post-detail'),
 ]
+
+urlpatterns += router.urls
