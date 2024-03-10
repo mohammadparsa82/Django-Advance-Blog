b6aef339efb98ed67adfce97cbd4be03a78ebf33
mohammad <121767738+mohammadparsa82@users.noreply.github.com>
2024-03-09 23:51:34 -0800

    commit ordering

--- core/blog/api/v1/views.py
+++ core/blog/api/v1/views.py
@@ -10,7 +10,7 @@ from rest_framework.views import APIView
 from rest_framework.generics import GenericAPIView , ListAPIView ,CreateAPIView , RetrieveUpdateDestroyAPIView
 from .permissions import IsOwnerReadOnly
 from django_filters.rest_framework import DjangoFilterBackend
-from rest_framework.filters import SearchFilter  
+from rest_framework.filters import SearchFilter  , OrderingFilter
 
 
 """
@@ -114,9 +114,10 @@ class PostModelViewsSet(viewsets.ModelViewSet):
     permission_classes = [IsAuthenticatedOrReadOnly,IsOwnerReadOnly]
     serializer_class = PostSerializer
     queryset = Post.objects.filter(status=True)
-    filter_backends = [DjangoFilterBackend,SearchFilter]
+    filter_backends = [DjangoFilterBackend,SearchFilter,OrderingFilter]
     filterset_fields = ['category','author']
     search_fields = ['title','content'] 
+    ordering_fields = ['published_data']
 
 class CategoryModelViewSet(viewsets.ModelViewSet):
     permission_classes = [IsAuthenticatedOrReadOnly]
