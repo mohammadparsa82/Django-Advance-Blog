ed85dc89ef49162669e57da244e86e880626787a
mohammad <121767738+mohammadparsa82@users.noreply.github.com>
2024-03-16 03:10:13 -0700

    commit Registration user

--- core/accounts/api/v1/urls.py
+++ core/accounts/api/v1/urls.py
@@ -1,6 +1,9 @@
 from django.urls import path , include
-
+from .views import * 
 app_name = "api-v1" 
 
 urlpatterns = [
+    #registration
+    path('registration/', RegistrationApiView.as_view(),name='registration'),
+
 ]
